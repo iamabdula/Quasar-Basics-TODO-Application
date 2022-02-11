3a9f22b24fc881f08523125d43183a42b618bb73
iamabdula <abm8656@gmail.com>
2022-02-11 18:16:48 +0500

    todo application in Quasar

---
+++ src/pages/Help.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="q-pa-lg" >
+    <h3 class="q-mt-none">HELP!</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, incidunt quod! Dignissimos tempore unde asperiores earum veniam nam repudiandae fugit quasi excepturi, corrupti enim mollitia, explicabo eos in dicta possimus velit ullam ratione sequi voluptatibus temporibus quibusdam incidunt doloribus maxime? Est sint debitis molestias accusamus aut suscipit, assumenda mollitia pariatur?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius, incidunt quod! Dignissimos tempore unde asperiores earum veniam nam repudiandae fugit quasi excepturi, corrupti enim mollitia, explicabo eos in dicta possimus velit ullam ratione sequi voluptatibus temporibus quibusdam incidunt doloribus maxime? Est sint debitis molestias accusamus aut suscipit, assumenda mollitia pariatur?</p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+
+})
+</script>
